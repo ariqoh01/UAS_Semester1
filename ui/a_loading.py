a74a791b571394e9cf4bad90477e63d91193e676
ariqoh01 <92715909+ariqoh01@users.noreply.github.com>
2022-01-15 09:56:33 +0700

    commit pertama

--- ui/a_loading.py
+++ ui/a_loading.py
@@ -9,15 +9,15 @@ class load:
     def loading(self):
         done = False
 
-        def reza():
-            for eza in itertools.cycle(['|', '/', '-', '\\']):
+        def animate():
+            for c in itertools.cycle(['|', '/', '-', '\\']):
                 if done:
                     break
-                sys.stdout.write('\rMemperoses data ' + eza)
+                sys.stdout.write('\rMemperoses data ' + c)
                 sys.stdout.flush()
                 time.sleep(0.1)
 
-        t = threading.Thread(target=reza)
+        t = threading.Thread(target=animate)
         t.start()
 
         time.sleep(3)
